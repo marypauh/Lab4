e1b282fe2fbd6d2e320242d0b3098ba2adb3a54b
Óscar Trejos Esquivel <56702153+oscarandres2@users.noreply.github.com>
2020-06-18 14:20:00 -0600

    borrando todo

--- Singleton/src/main/main.java
+++ Singleton/src/main/main.java
@@ -1,14 +1,12 @@
 package main;
 
-import logicadenegocios.Abogado;
-import logicadenegocios.AbogadoSingleton;
+import logicadenegocios
+import logicadenegocios
 
 public class main {
 
 	public static void main(String[] args) {
-		Abogado abogado = AbogadoSingleton.obtenerInstancia();
-		abogado.casar("Carlos", "Sofia");
-		abogado.divorciar("Marta", "Pablo");
+
 	}
 
 }
