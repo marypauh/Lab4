655ec1443be85491e3662009490c654a759ebef2
marypauh <maripau8@gmail.com>
2020-06-18 14:20:47 -0600

    Revert "push pop"
    This reverts commit 923cdfe18f661f4dce73e835e6a992dce3060372.

--- Singleton/src/main/main.java
+++ Singleton/src/main/main.java
@@ -2,8 +2,6 @@ package main;
 
 import logicadenegocios.Abogado;
 import logicadenegocios.AbogadoSingleton;
-import logicadenegocios.Universidad;
-import logicadenegocios.UniversidadSingleton;
 
 public class main {
 
@@ -11,10 +9,6 @@ public class main {
 		Abogado abogado = AbogadoSingleton.obtenerInstancia();
 		abogado.casar("Carlos", "Sofia");
 		abogado.divorciar("Marta", "Pablo");
-		
-		Universidad universidad = UniversidadSingleton.obtenerInstancia();
-		universidad.graduar("Valeria");
-		universidad.reprobrar("Kevin");
 	}
 
 }
